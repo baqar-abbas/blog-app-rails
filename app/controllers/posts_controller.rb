e1456328d6bac3d5824548134fe5e1d644301aee
baqar-abbas <baqar007@hotmail.com>
2023-08-24 16:58:29 +0500

    added the Views associated with the Models and Controllers already created for the Blog App

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    user_id = params[:user_id]
+    @posts = Post.where(author_id: user_id)
+    @comments = Comment.all
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+    @comments = Comment.where(post_id: params[:id])
+  end
 end
