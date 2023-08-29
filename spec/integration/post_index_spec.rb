36070a33fafe3edee03effcd3d5635d6b4162851
baqar-abbas <baqar007@hotmail.com>
2023-08-29 13:33:28 +0500

    Created post index spec file and defined half tests and other half tests will be pushed by my coding partner

---
+++ spec/integration/post_index_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+RSpec.describe 'Post Index Page', type: :feature do
+    describe 'Viewing Post Index page' do
+        before(:each) do
+            @user1 = User.create(name: 'Tom', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.',
+                                 posts_counter: 0)
+            @first_post = Post.create(author: @user1, title: 'post1', text: 'This is my first post', comments_counter: 0,
+                 likes_counter: 0)
+            @comment1 = Comment.create(post: @first_post, author: @user1, Text: 'Hi Tom!, Nice comment')
+            visit user_posts_path(@user1)
+          end
+
+          it 'should show user name' do
+            expect(page).to have_content(@user1.name)
+          end
+
+          it 'should show user photo' do
+            expect(page).to have_selector('img[alt="User Profile Picture"]')
+          end
+
+          it 'should show posts_counter of clicked user' do
+            expect(page).to have_content(@user1.posts_counter)
+          end
+
+          it 'should show post title' do
+            expect(page).to have_content(@first_post.title)
+          end
+          
+          it 'should show post text' do
+            expect(page).to have_content(@first_post.text)
+          end
+
+          
+    end
+end
