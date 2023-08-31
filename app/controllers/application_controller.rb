7ce7793f14cf0d6d3e9888db0ab3e7bc4608c706
paul-tes <pauasca8462@gmail.com>
2023-08-31 16:33:51 +0300

    Remove currnet user method form application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-  # before_action :current_user
-  # def current_user
-  #   @current_user = User.first
-  # end
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
