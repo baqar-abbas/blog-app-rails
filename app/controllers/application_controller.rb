f8f188c191a9cfddf0672e6f5952d045b5b535f6
paul-tes <pauasca8462@gmail.com>
2023-08-31 16:18:39 +0300

    Add sanitization for input fields in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,18 @@ class ApplicationController < ActionController::Base
   # def current_user
   #   @current_user = User.first
   # end
-  
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    # devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :name, :password, :posts_counter)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :name, :password, :password_confirmation, :posts_counter, :current_password) }
+    # devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password)}
+ 
+  # devise_parameter_sanitizer.permit(:account_update) do |u| u.permit(:name, :email, :password, :current_password)
+  devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :name, :password, :password_confirmation, :posts_counter, :current_password)}
+  end
 end
